3eeecc7baa10163dec98c77a7f119521a4b654fd
Norman Noble <norman@mistanorbo.com>
2018-12-20 16:30:23 +1100

    Add solution

--- Kata.Library/Kata.cs
+++ Kata.Library/Kata.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Kata.Library
 {
@@ -6,8 +8,31 @@ namespace Kata.Library
     {
         public static bool comp(int[] a, int[] b)
         {
-            // your code
-            return true;
+            if (a==null || b==null){
+                return false;
+            }
+
+            var listA = new List<int>(a);
+            var listB = new List<int>(b);
+
+            if (listA.Count()==0 && listB.Count()==0){
+                return true;
+            }
+            
+            if (listA.Count() != listB.Count()){
+                return false;
+            }
+
+            listA = listA.OrderBy(x => x).ToList();
+            listB = listB.OrderBy(x => x).ToList();
+            
+            if (listA.Max() > listB.Max()){
+                listB = listB.Select(x => x * x).ToList();
+            } else if (listB.Max() > listA.Max()) {
+                listA = listA.Select(x => x * x).ToList();
+            }
+
+            return listA.SequenceEqual(listB);
         }
     }
 }
--- Kata.Tests/KataTests.cs
+++ Kata.Tests/KataTests.cs
@@ -19,5 +19,22 @@ namespace Kata.Tests
             bool r = Kata.comp(a, b); // True
             Assert.AreEqual(true, r);
         }
+
+        [Test]
+        public void Test2()
+        {
+            int[] a = new int[] { 4, 4, 9 };
+            int[] b = new int[] { 4, 9, 9 };
+            bool r = Kata.comp(a, b); // True
+            Assert.AreEqual(false, r);
+        }
+
+        // [Test]
+        // public void Test2()
+        // {
+        //     int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+        //     int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+        //     Assert.AreEqual(true, b[0] == a[0]);
+        // }
     }
 }
